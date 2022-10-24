24c57fc8b38d71d6deee1ea31c06496ef68acf2c
Eugene Kazmin <varenkine@gmail.com>
2022-10-24 10:51:31 +0300

    security

--- src/main/java/ru/adrenoxxxrom/security/controller/MainController.java
+++ src/main/java/ru/adrenoxxxrom/security/controller/MainController.java
@@ -8,16 +8,9 @@ import java.security.Principal;
 @Controller
 public class MainController {
     private static final String REDIRECT_TO_LOGIN_PAGE = "redirect:/login";
-    private static final String REDIRECT_TO_ADMIN_PAGE = "redirect:/admin";
-    private static final String REDIRECT_TO_USER_PAGE = "redirect:/user";
 
     @GetMapping(value = "/")
     public String getRedirectToUserPage(Principal principal) {
-        if (principal == null) {
-            return REDIRECT_TO_LOGIN_PAGE;
-        } else if (principal.getName().equals("admin")) {
-            return REDIRECT_TO_ADMIN_PAGE;
-        }
-        return REDIRECT_TO_USER_PAGE;
+        return REDIRECT_TO_LOGIN_PAGE;
     }
 }
