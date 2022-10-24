ef9b4408a456fd28e2ce7175c309b655f0a33677
Eugene Kazmin <varenkine@gmail.com>
2022-10-24 10:52:46 +0300

    security

--- src/main/java/ru/adrenoxxxrom/security/controller/LoginController.java
+++ src/main/java/ru/adrenoxxxrom/security/controller/LoginController.java
@@ -3,14 +3,12 @@ package ru.adrenoxxxrom.security.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
-
 @Controller
-public class MainController {
+public class LoginController {
     private static final String REDIRECT_TO_LOGIN_PAGE = "redirect:/login";
 
     @GetMapping(value = "/")
-    public String getRedirectToUserPage(Principal principal) {
+    public String getRedirectToUserPage() {
         return REDIRECT_TO_LOGIN_PAGE;
     }
 }
